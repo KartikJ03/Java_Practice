8a521fcce8f4290f1682057d7af23418278d7ff1
KartikJ03 <kartikjawanjal.it@gmail.com>
2026-06-15 20:36:05 +0530

    Java Practice

---
+++ Java_Practice/Recursion_Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Recursion_Factorial {
+    static int factorial(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        return n*factorial(n-1);
+    }
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number = ");
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
+}
