a328c673754100cd88aa061683b61f7055b4d2e7
KartikJ03 <kartikjawanjal.it@gmail.com>
2026-08-09 17:03:28 +0530

    Java Practice

---
+++ Java_Practice/Self_Abs.java
@@ -0,0 +1,28 @@
+interface S1{
+    void meth1();
+    void meth2();
+}
+interface S2{
+    void meth3();
+    void meth4();
+}
+class final1 implements S2,S1{
+    public void meth1(){
+        System.out.println("meth1");
+    }
+    public void meth2(){
+        System.out.println("meth2");
+    }
+    public void meth3(){
+        System.out.println("meth3");
+    };
+    public void meth4(){
+        System.out.println("meth4");
+    }
+}
+public class Self_Abs {
+    public static void main (String [] args){
+        final1 f = new final1();
+        f.meth1();
+    }
+}
