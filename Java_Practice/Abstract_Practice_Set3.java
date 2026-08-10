6eb874e716afa26f519218b7b06f9355c39b4e33
KartikJ03 <kartikjawanjal.it@gmail.com>
2026-08-10 16:51:57 +0530

    Java Practice

--- Java_Practice/Abstract_Practice_Set3.java
+++ Java_Practice/Abstract_Practice_Set3.java
@@ -1,6 +1,31 @@
-
+interface basicAnimal{
+    void eat();
+    void sleep();
+}
+class monkey{
+    void jump(){
+        System.out.println("Jumping....");
+    }
+    void bite(){
+        System.out.println("biting....");
+    }
+}
+class human extends monkey implements basicAnimal{
+        void speak(){
+            System.out.println("Speaking....");
+        }
+        @Override
+    public void eat() {
+        System.out.println("eating");
+    }
+    @Override
+    public void sleep() {
+        System.out.println("Sleep");
+    }
+}
 public class Abstract_Practice_Set3 {
     public static void main(String [] args){
-
+        basicAnimal haryy = new human();
+        haryy.sleep();
     }
 }
