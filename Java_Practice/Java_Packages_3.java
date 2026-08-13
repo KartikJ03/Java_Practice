e8483c1d46fa51658f08eb2c73298a7f2431ca16
KartikJ03 <kartikjawanjal.it@gmail.com>
2026-08-13 15:11:37 +0530

    Java Practice

---
+++ Java_Practice/Java_Packages_3.java
@@ -0,0 +1,12 @@
+/*cretae a package named com.codewithkartik.shape
+* This package should have individual classes for rectangle, square circle cylinder
+* sphere
+* This classes should inheritance to properly manage the code
+* Include methods like volume surface area and getters and setters for dimensions
+* This code is written in VS Code for better understanding  */
+public class Java_Packages_3 {
+    public static void main(String [] args){
+        System.out.println("Please refer to VS Code (Shape.java)");
+
+    }
+}
