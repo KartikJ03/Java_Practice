8bd0c63e682ba2dbc6525b80cca482f354e45815
KartikJ03 <kartikjawanjal.it@gmail.com>
2026-08-13 13:28:14 +0530

    Java Practice

--- Java_Practice/Java_Package_1.java
+++ Java_Practice/Java_Package_1.java
@@ -1,5 +1,5 @@
 public class Java_Package_1 {
     public static void main(String [] args){
-        
+        System.out.println("Learning packages and working of java using VS-Code Terminal");
     }
 }
