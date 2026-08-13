c370132e2ac4700b138359a8d9bf66db68d7e097
KartikJ03 <kartikjawanjal.it@gmail.com>
2026-08-13 15:02:57 +0530

    Java Practice

---
+++ Java_Practice/Java_Packages_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+//import java.util.Scanner; //--> This is a built-in Package Utill-->Scanner for taking
+//user input
+public class Java_Packages_2 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any string");
+        String userInput = sc.nextLine();
+    }
+}
