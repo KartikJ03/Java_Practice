c83f62912db8c56adec8318d65cb2e1443ab1da5
KartikJ03 <kartikjawanjal.it@gmail.com>
2026-08-17 13:08:31 +0530

    Java Practice

---
+++ Java_Practice/Java_Practice_Test.java
@@ -0,0 +1,11 @@
+public class Java_Practice_Test {
+    public static void main(String [] args){
+        int n = 121;
+        int original = n;
+        int reverse = 0;
+        int digit = n /10;
+        System.out.println(digit);
+        }
+
+    }
+
