425c8b0454f7a2d077b0d7458d05b05f60a6e0d6
KartikJ03 <kartikjawanjal.it@gmail.com>
2026-08-17 13:08:16 +0530

    Java Practice

--- Java_Practice/Coding_Gpt_Test5.java
+++ Java_Practice/Coding_Gpt_Test5.java
@@ -1,9 +1,18 @@
 import java.util.*;
 public class Coding_Gpt_Test5 {
     public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number = ");
-        int n = sc.nextInt();
-
+        int n = 1231;
+        int reverse = 0;
+        int original = n;
+        while(n>0){
+            int digit = n % 10;
+            reverse = reverse *10 + digit;
+            n = n/10;
+        }
+        if (original==reverse){
+            System.out.println("Palindrome");
+        }else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
