3b7b71a645a3fd3c777b0dc3d9f9e6876311e25b
KartikJ03 <kartikjawanjal.it@gmail.com>
2026-08-22 23:48:46 +0530

    Java Practice

---
+++ Java_Practice/Java_Handling_Exception.java
@@ -0,0 +1,15 @@
+public class Java_Handling_Exception  {
+    public static void main(String [] args){
+        int a = 1000;
+        int b = 0;
+
+        try {
+            int c = a/b;
+            System.out.println("The Result is = "+c);
+        }catch (Exception e){
+            System.out.println("We failed to devide Reason: ");
+            System.out.println(e);
+        }
+        System.out.println("End Of Program");
+    }
+}
