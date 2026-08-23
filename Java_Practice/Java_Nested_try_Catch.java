7cda091af796735fe6a1f88e5c8f88b2ac53f355
KartikJ03 <kartikjawanjal.it@gmail.com>
2026-08-23 22:45:29 +0530

    Java Practice

---
+++ Java_Practice/Java_Nested_try_Catch.java
@@ -0,0 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Java_Nested_try_Catch {
+    public static void main(String [] args){
+        int [] marks = new int [3];
+        marks[0] = 65;
+        marks[1] = 54;
+        marks[2] = 45;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a index to display : ");
+        int ind = sc.nextInt();
+
+        try{
+            System.out.println("This a nested try catch explanation code");
+            try {
+                System.out.println("The Entered Index Is = "+marks[ind]);
+            }
+            catch (ArrayIndexOutOfBoundsException e ){
+                System.out.println("Please enter a valid index");
+                System.out.println("This is in the nested catch");
+            }
+        }
+        catch (InputMismatchException e){
+            System.out.println("Please enter a valid number");
+        }
+        catch (Exception e) {
+            System.out.println("Some Other Exception Occurred");
+        }
+        System.out.println("End of program");
+
+    }
+}
